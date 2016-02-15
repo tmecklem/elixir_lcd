38b4d56760b48352f8ae09b088f1d7692b474fe8
Timothy Mecklem <timothy@mecklem.com>
2016-02-15 06:29:07 +0000

    WIP Still trying to talk LCD

--- lib/elixir_lcd.ex
+++ lib/elixir_lcd.ex
@@ -1,89 +1,74 @@
 use Bitwise
 
 defmodule ElixirLcd do
-  # commands
-  @lcd_cleardisplay 0x01
-  @lcd_returnhome 0x02
-  @lcd_entrymodeset 0x04
-  @lcd_displaycontrol 0x08
-  @lcd_cursorshift 0x10
-  @lcd_functionset 0x20
-  @lcd_setcgramaddr 0x40
-  @lcd_setddramaddr 0x80
+  @instruction_clear_display 0x01
+  @instruction_return_home 0x02
+
+  @instruction_entry_mode_set 0x04
+  @ems_increment 0x02 # move cursor to right
+  @ems_shift_display 0x01 #scroll screen instead of move cursor
   
-  # flags for display entry mode
-  @lcd_entryright 0x00
-  @lcd_entryleft 0x02
-  @lcd_entryshiftincrement 0x01
-  @lcd_entryshiftdecrement 0x00
+  @instruction_display_control 0x08
+  @dc_display_on 0x04
+  @dc_cursor_on 0x02
+  @dc_blink_on 0x01
   
-  # flags for display on/off control
-  @lcd_displayon 0x04
-  @lcd_displayoff 0x00
-  @lcd_cursoron 0x02
-  @lcd_cursoroff 0x00
-  @lcd_blinkon 0x01
-  @lcd_blinkoff 0x00
+  @instruction_cursor_shift 0x10
+  @cs_displaymove 0x08
+  @cs_moveright 0x04
   
-  # flags for display/cursor shift
-  @lcd_displaymove 0x08
-  @lcd_cursormove 0x00
-  @lcd_moveright 0x04
-  @lcd_moveleft 0x00
+  @instruction_function_set 0x20
+  @fs_8_bit_mode 0x10
+  @fs_2_line 0x08
+  @fs_5x10_dots 0x04
   
-  # flags for function set
-  @lcd_8bitmode 0x10
-  @lcd_4bitmode 0x00
-  @lcd_2line 0x08
-  @lcd_1line 0x00
-  @lcd_5x10dots 0x04
-  @lcd_5x8dots 0x00
+  @instruction_setcgramaddr 0x40
+  @instruction_setddramaddr 0x80
   
-  # flags for backlight control
-  @lcd_backlight 0x08
-  @lcd_nobacklight 0x00
+  @lcd_no_backlight 0x00
+
+  @clear_all @instruction_clear_display ||| @instruction_return_home
+  @enter_4_bit_mode 0x20
   
-  @en 0b00000100  # Enable bit
-  @rw 0b00000010  # Read/Write bit
-  @rs 0b00000001  # Register select bit
+  @lcd_backlight 0b00001000
+  @en            0b00000100  # Enable bit
+  @rw            0b00000010  # Read/Write bit
+  @rs            0b00000001  # Register select bit
 
   def connect do
     {:ok, pid} = I2c.start_link("i2c-0", 0x3f)
 
+    IO.puts(" bl | en | rs | data")
+    IO.puts("====================")
     # reset
-    write_4_bits(pid, 0x00, 0x00)
+    write_4_bits(pid, 0x00)
     :timer.sleep 1000
    
-    write_4_bits(pid, 0x03, 0x00)
-    :timer.sleep 5 
-    write_4_bits(pid, 0x03, 0x00)
-    :timer.sleep 5 
-    write_4_bits(pid, 0x03, 0x00)
-    :timer.sleep 1 
+    #write_4_bits(pid, @clear_all)
+    #:timer.sleep 5 
+    #write_4_bits(pid, @clear_all)
+    #:timer.sleep 5 
+    #write_4_bits(pid, @clear_all)
+    #:timer.sleep 1 
     IO.puts "Entering 4-bit mode"
-    write_4_bits(pid, 0x02, 0x00)
+    write_4_bits(pid, @enter_4_bit_mode)
 
     IO.puts "Setting function set to 2 lines and 5x8 dots"
-    display_function = @lcd_4bitmode ||| @lcd_2line ||| @lcd_5x8dots
-    lcd_send(pid, @lcd_functionset ||| display_function)
+    lcd_send(pid, @instruction_function_set ||| @fs_2_line)
 
     IO.puts "Turning on display and cursor"
-    display_control = @lcd_displayon ||| @lcd_cursoron
-    lcd_send(pid, @lcd_displaycontrol ||| display_control)
+    display_control = @dc_display_on ||| @dc_cursor_on ||| @dc_blink_on
+    lcd_send(pid, @instruction_display_control ||| display_control)
 
-    IO.puts "Clearing display"
-    lcd_send(pid, @lcd_cleardisplay)
+    #IO.puts "Clearing display"
+    #lcd_send(pid, @instruction_clear_display)
 
     IO.puts "Setting entry left"
-    display_mode = @lcd_entryleft
-    lcd_send(pid, @lcd_entrymodeset ||| display_mode)
+    lcd_send(pid, @instruction_entry_mode_set ||| @ems_increment)
  
-    IO.puts "Returning cursor to home"
-    lcd_send(pid, @lcd_returnhome)
-
-    IO.puts "Printing 'Ready'"
-    :timer.sleep 200
-    write_chars(pid, 'Ready', 1)
+    # IO.puts "Printing 'Ready'"
+    # :timer.sleep 200
+    # write_chars(pid, 'Ready', 1)
   end
 
   def write_chars(pid, char_list, line) do
@@ -104,16 +89,20 @@ defmodule ElixirLcd do
   end
 
   defp lcd_send(pid, data, mode \\ 0) do
-    high = data &&& 0xf0 ||| mode
-    low = (data <<< 4) &&& 0xf0 ||| mode
-    # :io.format("sending ~8.2.0B ~8.2.0B (0x~2.16.0B~2.16.0B)~n", [high, low, high, low])
+    high = (data &&& 0xf0) ||| mode
+    low = ((data <<< 4) &&& 0xf0) ||| mode
     write_4_bits(pid, high) 
     write_4_bits(pid, low) 
   end
 
-  defp write_4_bits(pid, data, include_backlight \\ @lcd_backlight) do
-    data = data ||| include_backlight
-    :io.format("sending ~8.2.0B (0x~2.16.0B)~n", [data, data])
+  defp put_debug(data) do
+    backlight = (data &&& @lcd_backlight) >>> 3
+    enable_bit = (data &&& @en) >>> 2
+    register = (data &&& @rs)
+    :io.format("  ~w |  ~w |  ~w | ~4.2.0B~n", [backlight, enable_bit, register, data >>> 4])
+  end 
+
+  defp write_4_bits(pid, data) do
     i2c_expander_write(pid, data)
     pulse_enable(pid, data)
   end
@@ -126,7 +115,9 @@ defmodule ElixirLcd do
   end
 
   defp i2c_expander_write(pid, data) do
-    I2c.write(pid, <<data>>)
+    data_with_backlight = data ||| @lcd_backlight
+    put_debug(data_with_backlight)
+    I2c.write(pid, <<data_with_backlight>>)
     :timer.sleep 1
   end
 end
